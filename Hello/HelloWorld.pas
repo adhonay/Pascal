e63e581708a177dcad607b9e5ca0ef8f18982883
Luizgustavo358 <luiz.braganca@sga.pucminas.br>
2018-09-02 14:15:57 -0300

    fist commit

---
+++ Hello/HelloWorld.pas
@@ -0,0 +1,13 @@
+{+------------------------------------+
+ |  Exemplo de Programa em Pascal.    |
+ +------------------------------------+
+ |  feito por: Luiz Gustavo           |
+ -------------------------------------+}
+
+{ Nome do programa }
+program HelloWorld;
+
+begin
+    { Escreve no terminal }
+    WriteLn('Hello World');
+end.
